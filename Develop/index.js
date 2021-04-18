596d7515ee0fe95a9fe72a0c46954eeb79c90300
Syed Tirmizi <syedmtirmizi@gmail.com>
2021-04-18 03:01:10 -0400

    enable generation of readme with data

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,88 @@
+// TODO: Create a function to initialize app
+function init() {
+
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generate = require('./utils/generateMarkdown');
+const path = require('path');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is the description of your project?'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How do you install your project?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What is the usage scenario for your project?'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'What are your contribution guidelines?'
+    },
+    {
+        type: 'input',
+        name: 'testing',
+        message: 'What are your testing instructions?'
+    },
+    {
+        type:'list',
+        name: 'license',
+        message: 'What type of license does your applicaiton use?',
+        choices: ['Community', 'MIT', 'GNU GPLv3']
+    },
+    {
+        type:'input',
+        name: 'github',
+        message: 'What is your GitHub username?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your e-mail address?'
+    }
+];
+
+inquirer
+    .prompt(questions)
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+.then(function (data) {
+    const fileName =
+        data.title
+            .toLowerCase()
+            .split(' ')
+            .join('') + '.md';
 
-// TODO: Create a function to initialize app
-function init() {}
+    console.log(data);
+
+    fs.writeFileSync(path.join (process.cwd(), fileName), generate(data));
+
+    // fs.writeFile(fileName, JSON.stringify(data, null, '\t'), function(err) {
+    //     if (err) {
+    //         return console.log(err);
+    //     }
+
+    //     console.log('Success!');
+    // })
+}) 
+
+};
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -12,7 +12,16 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}, 
+  # ${data.description},
+  # ${data.installation},
+  # ${data.usage},
+  # ${data.contribution},
+  # ${data.testing},
+  # ${data.license},
+  # ${data.github},
+  # ${data.email}
 
 `;
 }
